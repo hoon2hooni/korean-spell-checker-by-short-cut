ec2b2171a4bc49777ac320d598dfb8be70958c2e
hoon2hooni <sanghoon.engineer@gmail.com>
2025-01-04 16:41:48 +0900

    chore: app생성요 setup py 추가

--- setup.py
+++ setup.py
@@ -1,43 +1,16 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
+from setuptools import setup
 
+APP = ['correct_korean.py']  # 변환할 스크립트
+OPTIONS = {
+    'argv_emulation': True, 
+    'plist': {
+        'LSBackgroundOnly': True,  # 백그라운드 전용 (창 없이 실행)
+    }
+}
 
-def install():
-    required = []
-    with open('requirements.txt') as f:
-        requirements = f.read().splitlines()
-        for req in requirements:
-            p = req.split('==')
-            required.append(p[0])
-    desc = ''
-    setup(
-        name='py-hanspell',
-        version='1.1',
-        description=desc,
-        long_description=desc,
-        author='SuHun Han',
-        author_email='ssut@ssut.me',
-        url='https://github.com/ssut/py-hanspell',
-        classifiers=[
-            'Development Status :: 5 - Production/Stable',
-            'Intended Audience :: Education',
-            'Intended Audience :: End Users/Desktop',
-            'License :: Freeware',
-            'Operating System :: POSIX',
-            'Operating System :: Microsoft :: Windows',
-            'Operating System :: MacOS :: MacOS X',
-            'Topic :: Education',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 2.6',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3.2',
-            'Programming Language :: Python :: 3.3',
-            'Programming Language :: Python :: 3.4'
-        ],
-        packages=find_packages(),
-        install_requires=required,
-    )
-
-
-if __name__ == "__main__":
-    install()
+setup(
+    app=APP,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
